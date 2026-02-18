d282928a98154a107a12d215ee4229c1510f07b4
Joon Hao <52413727+bjh-developer@users.noreply.github.com>
2026-02-18 16:17:34 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,12 +62,12 @@ export default function RootLayout({
             name="google-site-verification"
             content="nO-YAzyQoB0NZ75BCn7gL3M8SK8u-hPG52ShKXrfshY"
           />
+          <script
+            src="https://analytics.ahrefs.com/analytics.js"
+            data-key="bvLONNN9gpGp6wB1c+Aakw"
+            async
+          ></script>
         </head>
-        <script
-          src="https://analytics.ahrefs.com/analytics.js"
-          data-key="bvLONNN9gpGp6wB1c+Aakw"
-          async
-        ></script>
         <body
           className={`${playfairDisplay.variable} ${inter.variable} ${caveat.variable} antialiased h-full`}
         >
