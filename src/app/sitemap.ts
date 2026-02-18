89c4ee79131c97fabb3e110104c61c5b968bf36e
Joon Hao <52413727+bjh-developer@users.noreply.github.com>
2026-02-18 15:57:51 +0800

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,12 +3,6 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://restrip.app/',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 1,
-    },
-    {
       url: 'https://www.restrip.app/',
       lastModified: new Date(),
       changeFrequency: 'monthly',
